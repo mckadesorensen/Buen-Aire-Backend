eb08a89676eef072251152426e17a5a06a687ad0
McKade Sorensen <dmsorensen@alaska.edu>
2021-03-24 12:06:44 -0800

    Added IAM lambda role and the process_data lambda

---
+++ workflow/main.tf
@@ -0,0 +1,43 @@
+terraform {
+  backend "s3"{}
+}
+
+provider "aws" {}
+
+locals {
+  prefix = "${var.DEPLOY_NAME}-buen-aire-"
+  python_version = "python3.8"
+}
+
+# ------------------ Start of IAM ------------------
+resource "aws_iam_role" "lambda_iam_role" {
+  name = "${local.prefix}lambda_iam_role"
+  assume_role_policy = <<EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Action": "sts:AssumeRole",
+        "Principal": {
+          "Service": "lambda.amazonaws.com"
+        },
+        "Effect": "Allow",
+        "Sid": ""
+      }
+    ]
+  }
+  EOF
+}
+
+# ------------------ End of IAM ------------------
+
+
+
+# ------------------ Start of Lambdas ------------------
+resource "aws_lambda_function" "process_data" {
+  function_name = "${local.prefix}process_data"
+  handler = "lambdas.process_data.lambda_handler"
+  role = aws_iam_role.lambda_iam_role.arn
+  runtime = local.python_version
+}
+# ------------------ End of Lambdas ------------------
