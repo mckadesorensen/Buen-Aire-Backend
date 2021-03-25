3e1dfbe76864ba4223856f622e4e56b950977fd0
McKade Sorensen <dmsorensen@alaska.edu>
2021-03-25 15:30:18 -0800

    Added the skeleton of the API Gateway

--- workflow/main.tf
+++ workflow/main.tf
@@ -63,3 +63,40 @@ resource "aws_lambda_function" "egress_data" {
   runtime = local.python_version
 }
 # ------------------ End of Lambdas ------------------
+
+# ------------------ Start of API Gateway ------------------
+# TODO: Make this do something
+resource "aws_api_gateway_rest_api" "api" {
+  name = "${local.prefix}-api-gateway"
+  description = "Proxy for handling request to the Buen Aire API"
+}
+
+resource "aws_api_gateway_resource" "resource" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  path_part   = "{proxy+}"
+}
+
+resource "aws_api_gateway_method" "method" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.resource.id
+  http_method = "ANY"
+  authorization = "NONE"
+  request_parameters = {
+    "method.request.path.proxy" = true
+  }
+}
+resource "aws_api_gateway_integration" "integration" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.resource.id
+  http_method = aws_api_gateway_method.method.http_method
+  integration_http_method = "ANY"
+  type = "HTTP_PROXY"
+  uri = "http://your.domain.com/{proxy}"
+
+  request_parameters =  {
+    "integration.request.path.proxy" = "method.request.path.proxy"
+  }
+}
+
+# ------------------ End of API Gateway ------------------
