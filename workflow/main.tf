d63a0427e4bd193451f56b60d5742e5523947cd8
McKade Sorensen <dmsorensen@alaska.edu>
2021-03-24 17:59:44 -0800

    Commenting out lambdas for the time being

--- workflow/main.tf
+++ workflow/main.tf
@@ -7,6 +7,8 @@ provider "aws" {}
 locals {
   prefix = "${var.DEPLOY_NAME}-buen-aire-"
   python_version = "python3.8"
+  zipped_lambdas = "lambdas.zip"
+  zipped_lambda_bucket = "s3://${var.DEPLOY_NAME}-buen-aire-lambda-code-${var.AWS_ID_LAST_FOUR}"
 }
 
 # ------------------ Start of IAM ------------------
@@ -34,17 +36,22 @@ EOF
 
 
 # ------------------ Start of Lambdas ------------------
-resource "aws_lambda_function" "process_data" {
-  function_name = "${local.prefix}process_data"
-  handler = "lambdas.process_data.lambda_handler"
-  role = aws_iam_role.lambda_iam_role.arn
-  runtime = local.python_version
-}
-
-resource "aws_lambda_function" "egress_data" {
-  function_name = "${local.prefix}egress_data"
-  handler = "lambdas.process_data.lambda_handler"
-  role = aws_iam_role.lambda_iam_role.arn
-  runtime = local.python_version
-}
+//resource "aws_lambda_function" "process_data" {
+//  s3_bucket =  local.zipped_lambda_bucket
+//  s3_key = local.zipped_lambdas
+//  function_name = "${local.prefix}process_data"
+//  handler = "workflow.lambdas.process_data.lambda_handler"
+//  role = aws_iam_role.lambda_iam_role.arn
+//  runtime = local.python_version
+//}
+//
+//resource "aws_lambda_function" "egress_data" {
+//  s3_bucket =  local.zipped_lambda_bucket
+//  s3_key = local.zipped_lambdas
+//
+//  function_name = "${local.prefix}egress_data"
+//  handler = "workflow.lambdas.egress_data.lambda_handler"
+//  role = aws_iam_role.lambda_iam_role.arn
+//  runtime = local.python_version
+//}
 # ------------------ End of Lambdas ------------------
