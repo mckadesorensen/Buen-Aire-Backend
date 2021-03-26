37930002e25ef5aefe50793fbf69e2cea35931dc
McKade Sorensen <dmsorensen@alaska.edu>
2021-03-26 10:00:51 -0800

    cleaned up file

--- workflow/lambdas/process_data.py
+++ workflow/lambdas/process_data.py
@@ -6,24 +6,40 @@ import boto3
 from purpleair.network import SensorList
 
 
+s3 = boto3.resource("s3")
+
+
+# --------------- Start of s3 formatting functions ---------------
+# TODO: Get with team and figure out how we want to properly format this data
 def format_data(purple_air, uaf_smoke):
     return dumps({
         "PurpleAir": purple_air,
         "UAFSmoke": uaf_smoke
     })
+# --------------- End of s3 formatting functions ---------------
 
 
-def store_in_s3():
-    purple_air_data = get_data_from_purple_air()
-    uaf_smoke_data = "TODO"
-    data = format_data(purple_air_data, uaf_smoke_data)
-    encoded_data = data.encode("utf-8")
+# --------------- Start of s3 functions ---------------
+# TODO: Add logging and log response
+def set_file_public_read_access(bucket, file_name):
+    object_acl = s3.ObjectAcl(bucket, file_name)
+    response = object_acl.put(ACL='public-read')
 
+
+# TODO: Set up file name
+def store_file_in_s3(data):
     bucket = os.getenv("S3_DATA_BUCKET")
     file_name = "test.json"
 
-    s3 = boto3.resource("s3")
-    s3.Bucket(bucket).put_object(Key=file_name, Body=encoded_data)
+    s3.Bucket(bucket).put_object(Key=file_name, Body=data)
+    set_file_public_read_access(bucket, file_name)
+# --------------- Start of s3 functions ---------------
+
+
+# --------------- Start of gathering data functions ---------------
+# TODO: Setup to get UAF Smoke data
+def get_data_from_uaf_smoke():
+    return "UAF Smoke data"
 
 
 def get_data_from_purple_air():
@@ -32,5 +48,13 @@ def get_data_from_purple_air():
     return df.to_json(orient="records")
 
 
+def pull_data_from_all_sources():
+    purple_air_data = get_data_from_purple_air()
+    uaf_smoke_data = get_data_from_uaf_smoke()
+    encoded_data = format_data(purple_air_data, uaf_smoke_data).encode("utf-8")
+    store_file_in_s3(encoded_data)
+# --------------- End of gathering data functions ---------------
+
+
 def lambda_handler(event, context):
-    store_in_s3()
+    pull_data_from_all_sources()
