eced7e6d6c81733ad143e1cee8a736e8c0f1097a
McKade Sorensen <dmsorensen@alaska.edu>
2021-04-08 16:44:53 -0800

    Added logging

--- workflow/lambdas/egress_data.py
+++ workflow/lambdas/egress_data.py
@@ -1,24 +1,28 @@
 import boto3
 import os
 import json
+import logging
 
 
-# TODO: Set up with API Gateway
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
 def create_recent_data_url():
     bucket, region = os.getenv("S3_DATA_BUCKET"), os.getenv("BUCKET_REGION")
     s3_client = boto3.client('s3')
     files = s3_client.list_objects_v2(Bucket=bucket)["Contents"]
-    print(files)
 
     for file in files:
         data = s3_client.get_object(Bucket=bucket, Key=file.get('Key'))
         contents = data['Body'].read()
 
+    logging.info(f"Sending user the following data: {data}")
     return contents.decode('UTF-8')
 
 
 def lambda_handler(event, context):
     return {
         "statusCode": 200,
-        "body": json.dumps(create_recent_data_url())
+        "body": create_recent_data_url()
     }
